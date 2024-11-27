bbb898765e6007ab56441f4816307a42e9513030
Bartosz Wątorek <b_watorek@inf15.local>
2024-11-27 09:05:35 +0100

    Login +  Registration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  //   import "@/static/shared.css";
+  import "../app.css";
 </script>
 
 <slot />
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,16 +5,14 @@
   <form action="post" class="flex flex-col gap-4 bg-slate-800 p-5 rounded-xl">
     <label for="">Email</label>
     <input type="email" />
-    <label for="">Name</label>
-    <input type="text" />
     <label for="">Password</label>
     <input type="password" />
-    <label for="">Repeat password</label>
-    <input type="password" />
 
-    <button type="submit">Register</button>
+    <button type="submit">Login</button>
     <p class="text-center text-white">
-      Already have an account? <a href="/login" class="text-blue-500">Login</a>
+      Don't have an account? <a href="/register" class="text-blue-500"
+        >Register</a
+      >
     </p>
   </form>
 </main>
